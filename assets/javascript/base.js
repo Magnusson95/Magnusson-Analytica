8c935b1d267be51c52e4845cae266b136cf30a66
Magnusson95 <alexander.magnusson95@gmail.com>
2020-04-26 17:56:41 +0300

    Animating software toggle

--- assets/javascript/base.js
+++ assets/javascript/base.js
@@ -9,3 +9,91 @@ $(".callback").click(function () {
 $(".close").click(function () {
   $(".callback-form").toggle("slow");
 });
+
+$("#tableau-logo").click(function () {
+  $(".my-software__collage__container")
+    .addClass("animated")
+    .addClass("flipOutX");
+});
+
+$("#power-bi-logo").click(function () {
+  $(".my-software__collage__container")
+    .addClass("animated")
+    .addClass("flipOutX");
+});
+
+$("#html-logo").click(function () {
+  $(".my-software__collage__container")
+    .addClass("animated")
+    .addClass("flipOutX");
+});
+
+$("#css-logo").click(function () {
+  $(".my-software__collage__container")
+    .addClass("animated")
+    .addClass("flipOutX");
+});
+
+$("#javascript-logo").click(function () {
+  $(".my-software__collage__container")
+    .addClass("animated")
+    .addClass("flipOutX");
+});
+
+$("#jQuery-logo").click(function () {
+  $(".my-software__collage__container")
+    .addClass("animated")
+    .addClass("flipOutX");
+});
+
+$("#vs-logo").click(function () {
+  $(".my-software__collage__container")
+    .addClass("animated")
+    .addClass("flipOutX");
+});
+
+$("#sql-logo").click(function () {
+  $(".my-software__collage__container")
+    .addClass("animated")
+    .addClass("flipOutX");
+});
+
+$("#excel-logo").click(function () {
+  $(".my-software__collage__container")
+    .addClass("animated")
+    .addClass("flipOutX");
+});
+
+$("#access-logo").click(function () {
+  $(".my-software__collage__container")
+    .addClass("animated")
+    .addClass("flipOutX");
+});
+
+$("#alteryx-logo").click(function () {
+  $(".my-software__collage__container")
+    .addClass("animated")
+    .addClass("flipOutX");
+});
+
+$("#sap-logo").click(function () {
+  $(".my-software__collage__container")
+    .addClass("animated")
+    .addClass("flipOutX");
+});
+
+$("#scrum-logo").click(function () {
+  $(".my-software__collage__container")
+    .addClass("animated")
+    .addClass("flipOutX");
+});
+
+$("#jira-logo").click(function () {
+  $(".my-software__collage__container")
+    .addClass("animated")
+    .addClass("fadeOutLeft");
+  $(".software-details")
+    .removeClass("hidden")
+    .addClass("animated")
+    .addClass("fadeInRight");
+});
