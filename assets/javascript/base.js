edf60dbdc09546d9d6c132aaa1723455819846e0
Magnusson95 <alexander.magnusson95@gmail.com>
2020-04-26 17:17:19 +0300

    Callback form animation improved

--- assets/javascript/base.js
+++ assets/javascript/base.js
@@ -1,3 +1,11 @@
 $(".header__hamburger").click(function () {
   $(".header__links").toggle("slow");
 });
+
+$(".callback").click(function () {
+  $(".callback-form").toggle("slow");
+});
+
+$(".close").click(function () {
+  $(".callback-form").toggle("slow");
+});
