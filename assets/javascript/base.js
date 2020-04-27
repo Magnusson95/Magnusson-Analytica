727c53c93530c40e957a36f18ecd356e132c3c9f
Magnusson95 <alexander.magnusson95@gmail.com>
2020-04-27 14:28:35 +0300

    Software Details Animation Transition Updated

--- assets/javascript/base.js
+++ assets/javascript/base.js
@@ -10,87 +10,212 @@ $(".close").click(function () {
   $(".callback-form").toggle("slow");
 });
 
+$(".my-software__details__back").click(function () {
+  $(".my-software__details")
+    .removeClass("animated")
+    .removeClass("fadeIn")
+    .addClass("animated")
+    .addClass("fadeOut")
+    .removeClass("top");
+  $(".my-software__collage__container")
+    .removeClass("animated")
+    .removeClass("fadeOut")
+    .addClass("animated")
+    .addClass("fadeIn");
+});
+
 $("#tableau-logo").click(function () {
   $(".my-software__collage__container")
+    .removeClass("animated")
+    .removeClass("fadeIn")
     .addClass("animated")
-    .addClass("flipOutX");
-  $(".my-software__details").addClass("animated").addClass("flipInX");
+    .addClass("fadeOut");
+  $("#tableau-details")
+    .removeClass("animated")
+    .removeClass("fadeOut")
+    .addClass("animated")
+    .addClass("fadeIn")
+    .addClass("top");
 });
 
 $("#power-bi-logo").click(function () {
   $(".my-software__collage__container")
+    .removeClass("animated")
+    .removeClass("fadeIn")
     .addClass("animated")
-    .addClass("flipOutX");
+    .addClass("fadeOut");
+  $("#power-bi-details")
+    .removeClass("animated")
+    .removeClass("fadeOut")
+    .addClass("animated")
+    .addClass("fadeIn")
+    .addClass("top");
 });
 
 $("#html-logo").click(function () {
   $(".my-software__collage__container")
+    .removeClass("animated")
+    .removeClass("fadeIn")
     .addClass("animated")
-    .addClass("flipOutX");
+    .addClass("fadeOut");
+  $("#html-details")
+    .removeClass("animated")
+    .removeClass("fadeOut")
+    .addClass("animated")
+    .addClass("fadeIn")
+    .addClass("top");
 });
 
 $("#css-logo").click(function () {
   $(".my-software__collage__container")
+    .removeClass("animated")
+    .removeClass("fadeIn")
     .addClass("animated")
-    .addClass("flipOutX");
+    .addClass("fadeOut");
+  $("#css-details")
+    .removeClass("animated")
+    .removeClass("fadeOut")
+    .addClass("animated")
+    .addClass("fadeIn")
+    .addClass("top");
 });
 
 $("#javascript-logo").click(function () {
   $(".my-software__collage__container")
+    .removeClass("animated")
+    .removeClass("fadeIn")
     .addClass("animated")
-    .addClass("flipOutX");
+    .addClass("fadeOut");
+  $("#javascript-details")
+    .removeClass("animated")
+    .removeClass("fadeOut")
+    .addClass("animated")
+    .addClass("fadeIn")
+    .addClass("top");
 });
 
 $("#jQuery-logo").click(function () {
   $(".my-software__collage__container")
+    .removeClass("animated")
+    .removeClass("fadeIn")
     .addClass("animated")
-    .addClass("flipOutX");
+    .addClass("fadeOut");
+  $("#jQuery-details")
+    .removeClass("animated")
+    .removeClass("fadeOut")
+    .addClass("animated")
+    .addClass("fadeIn")
+    .addClass("top");
 });
 
 $("#vs-logo").click(function () {
   $(".my-software__collage__container")
+    .removeClass("animated")
+    .removeClass("fadeIn")
     .addClass("animated")
-    .addClass("flipOutX");
+    .addClass("fadeOut");
+  $("#vs-details")
+    .removeClass("animated")
+    .removeClass("fadeOut")
+    .addClass("animated")
+    .addClass("fadeIn")
+    .addClass("top");
 });
 
 $("#sql-logo").click(function () {
   $(".my-software__collage__container")
+    .removeClass("animated")
+    .removeClass("fadeIn")
     .addClass("animated")
-    .addClass("flipOutX");
+    .addClass("fadeOut");
+  $("#sql-details")
+    .removeClass("animated")
+    .removeClass("fadeOut")
+    .addClass("animated")
+    .addClass("fadeIn")
+    .addClass("top");
 });
 
 $("#excel-logo").click(function () {
   $(".my-software__collage__container")
+    .removeClass("animated")
+    .removeClass("fadeIn")
     .addClass("animated")
-    .addClass("flipOutX");
+    .addClass("fadeOut");
+  $("#excel-details")
+    .removeClass("animated")
+    .removeClass("fadeOut")
+    .addClass("animated")
+    .addClass("fadeIn")
+    .addClass("top");
 });
 
 $("#access-logo").click(function () {
   $(".my-software__collage__container")
+    .removeClass("animated")
+    .removeClass("fadeIn")
     .addClass("animated")
-    .addClass("flipOutX");
+    .addClass("fadeOut");
+  $("#access-details")
+    .removeClass("animated")
+    .removeClass("fadeOut")
+    .addClass("animated")
+    .addClass("fadeIn")
+    .addClass("top");
 });
 
 $("#alteryx-logo").click(function () {
   $(".my-software__collage__container")
+    .removeClass("animated")
+    .removeClass("fadeIn")
     .addClass("animated")
-    .addClass("flipOutX");
+    .addClass("fadeOut");
+  $("#alteryx-details")
+    .removeClass("animated")
+    .removeClass("fadeOut")
+    .addClass("animated")
+    .addClass("fadeIn")
+    .addClass("top");
 });
 
 $("#sap-logo").click(function () {
   $(".my-software__collage__container")
+    .removeClass("animated")
+    .removeClass("fadeIn")
     .addClass("animated")
-    .addClass("flipOutX");
+    .addClass("fadeOut");
+  $("#sap-details")
+    .removeClass("animated")
+    .removeClass("fadeOut")
+    .addClass("animated")
+    .addClass("fadeIn")
+    .addClass("top");
 });
 
 $("#scrum-logo").click(function () {
   $(".my-software__collage__container")
+    .removeClass("animated")
+    .removeClass("fadeIn")
     .addClass("animated")
-    .addClass("flipOutX");
+    .addClass("fadeOut");
+  $("#scrum-details")
+    .removeClass("animated")
+    .removeClass("fadeOut")
+    .addClass("animated")
+    .addClass("fadeIn")
+    .addClass("top");
 });
 
 $("#jira-logo").click(function () {
   $(".my-software__collage__container")
+    .removeClass("animated")
+    .removeClass("fadeIn")
     .addClass("animated")
-    .addClass("flipOutX");
+    .addClass("fadeOut");
+  $("#jira-details")
+    .removeClass("animated")
+    .removeClass("fadeOut")
+    .addClass("animated")
+    .addClass("fadeIn")
+    .addClass("top");
 });
