672a6e403368806d4f9d90d73de24477922595b7
Magnusson95 <alexander.magnusson95@gmail.com>
2020-05-04 09:06:16 +0300

    Added link to first blog, added a new sticky header for blogs

--- assets/javascript/base.js
+++ assets/javascript/base.js
@@ -15,6 +15,31 @@ $(".close").click(function () {
   $(".subscribe-form").toggle("slow");
 });
 
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+  myFunction();
+};
+
+// Get the header
+var header = document.getElementById("primaryHeader");
+var newheader = document.getElementById("secondaryHeader");
+var oldhamburger = document.getElementById("primaryHamburger");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    newheader.classList.remove("remove");
+    oldhamburger.classList.add("remove");
+  } else {
+    header.classList.remove("remove");
+    newheader.classList.add("remove");
+    oldhamburger.classList.remove("remove");
+  }
+}
+
 $(".my-software__details__back").click(function () {
   $(".my-software__details")
     .removeClass("animated")
